6e0f01682799966f8ae823408f37942f392a43cb
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-01-22 11:38:53 +0530

    Update 1856_Maximum_Subarray_Min-Product.java

--- WEEK_2/1856_Maximum_Subarray_Min-Product.java
+++ WEEK_2/1856_Maximum_Subarray_Min-Product.java
@@ -19,37 +19,36 @@ class Solution {
 // OPTIMAL APPROACH
 class Solution {
     public int maxSumMinProduct(int[] nums) {
-        long[] pre = new long[nums.length+1];
-        long ans = 0;
-        for(int i = 0;i<nums.length;i++){
-            pre[i+1] = pre[i]+nums[i];
+        long[] pre=new long[nums.length+1];
+        long ans=0;
+        for(int i=0;i<nums.length;i++){
+            pre[i+1]=pre[i]+nums[i];
         }
-        Stack<Integer> st = new Stack<>();
-        int[] pse = new int[nums.length];
-        for(int i = 0;i<nums.length;i++){
+        Stack<Integer> st=new Stack<>();
+        int[] pse=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
             while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
                 st.pop();
             }
-            pse[i] = st.isEmpty()?-1:st.peek();
+            pse[i]=st.isEmpty()?-1:st.peek();
             st.push(i);
         }
         st.clear();
-        int[] nxe = new int[nums.length];
-        for(int i = nums.length-1;i>=0;i--){
+        int[] nxe=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
              while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
                 st.pop();
             }
-            nxe[i] = st.isEmpty()?nums.length:st.peek();
+            nxe[i]=st.isEmpty()?nums.length:st.peek();
             st.push(i);
         }
-        for(int i = 0;i<nums.length;i++){
-            int l = pse[i]+1;  
-            int r = nxe[i]-1;
-            long sum =  pre[r+1]-pre[l];
-            long subarray = sum * nums[i];
-            ans = Math.max(ans,subarray);
+        for(int i=0;i<nums.length;i++){
+            int l=pse[i]+1;  
+            int r=nxe[i]-1;
+            long sum=pre[r+1]-pre[l];
+            long subarray=sum*nums[i];
+            ans=Math.max(ans,subarray);
         }
         return (int)(ans%1_000_000_007);
-
     }
 }
